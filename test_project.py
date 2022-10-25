a0a30e874cf1ccae7a477c80fdbd17ede4c5fe57
Josue Martinez <josue-dev@outlook.com>
2022-10-25 17:49:56 +0000

    Add tests for mock functions

--- test_project.py
+++ test_project.py
@@ -1,11 +1,112 @@
 import os
-from project import get_command_and_device, get_device_dict
+from project import (
+    mock_message_debug,
+    mock_message_help,
+    mock_send_device_output,
+    get_command_and_device,
+    get_device_dict,
+)
 from netbot import NetBot
 
 
 # set environment variables for logging into devices
 os.environ["NTWK_USERNAME"] = "tester"
 os.environ["NTWK_PASSWORD"] = "testerpw"
+
+
+def test_mock_message_debug():
+    message = {
+        "client_msg_id": "02e747f5-6770-4fe4-80fe-c549dde1ca20",
+        "type": "message",
+        "text": "netbot debug",
+        "user": "U046H2Q5JAK",
+        "ts": "1666716633.386989",
+        "blocks": [
+            {
+                "type": "rich_text",
+                "block_id": "L7Li",
+                "elements": [
+                    {
+                        "type": "rich_text_section",
+                        "elements": [{"type": "text", "text": "netbot debug"}],
+                    }
+                ],
+            }
+        ],
+        "team": "T046R0Z54TG",
+        "channel": "C047A88DJC9",
+        "event_ts": "1666716633.386989",
+        "channel_type": "channel",
+    }
+
+    assert mock_message_debug(message) == (
+        "```\n"
+        "client_msg_id: 02e747f5-6770-4fe4-80fe-c549dde1ca20\n"
+        "type: message\n"
+        "text: netbot debug\n"
+        "user: U046H2Q5JAK\n"
+        "ts: 1666716633.386989\n"
+        "blocks: [{'type': 'rich_text', 'block_id': 'L7Li', 'elements': [{'type': 'rich_text_section', 'elements': [{'type': 'text', 'text': 'netbot debug'}]}]}]\n"
+        "team: T046R0Z54TG\n"
+        "channel: C047A88DJC9\n"
+        "event_ts: 1666716633.386989\n"
+        "channel_type: channel\n\n"
+        "```"
+    )
+
+
+def test_mock_message_help():
+    # the msg text contents doesn't matter here
+    # because it's parsed by the decorator
+    assert mock_message_help("foo") == (
+        "Here are the things you can ask me to do:\n\n"
+        "```\n"
+        "# get interface information\n"
+        "netbot get interface info device=<device name>\n\n"
+        "# get routes\n"
+        "netbot get routes device=<device name>\n\n"
+        "# get HSRP brief output\n"
+        "netbot get hsrp device=<device name>\n"
+        "```\n\n"
+        "where devices are `rt1` and `rt2`"
+    )
+
+
+def test_mock_send_device_output_valid_device():
+    message = {"text": "netbot get interface info device=rt1"}
+
+    assert mock_send_device_output(message) == (
+        "```\n"
+        "Interface                  IP-Address      OK? Method Status                Protocol\n"
+        "GigabitEthernet0/0         192.168.128.119 YES DHCP   up                    up      \n"
+        "GigabitEthernet0/1         unassigned      YES NVRAM  up                    up      \n"
+        "GigabitEthernet0/2         unassigned      YES NVRAM  up                    up      \n"
+        "GigabitEthernet0/2.101     10.10.10.2      YES NVRAM  up                    up      \n"
+        "GigabitEthernet0/2.102     10.20.10.2      YES NVRAM  up                    up      \n"
+        "GigabitEthernet0/2.201     10.70.20.2      YES NVRAM  up                    up      \n"
+        "GigabitEthernet0/2.202     10.80.20.2      YES NVRAM  up                    up      \n"
+        "GigabitEthernet0/3         unassigned      YES NVRAM  administratively down down    \n"
+        "```"
+    )
+
+def test_mock_send_device_output_invalid_device():
+    message = {"text": "netbot get interface info device=rt3"}
+
+    assert mock_send_device_output(message) == (
+        '```\n'
+        'Invalid device. Say "netbot help" for list of valid devices.\n'
+        '```'
+        )
+
+def test_mock_send_device_output_invalid_command():
+    message = {"text": "netbot get foo device=rt1"}
+
+    assert mock_send_device_output(message) == (
+        "```\n"
+        "Command 'netbot get foo' is invalid or currently unsupported."
+        "Say `netbot help` for list of valid commands.\n"
+        "```"
+        )
 
 
 def test_get_device_dict_rt1():
